eed3c91e8056a09407a5ccae8ef032f478366614
Jacob Roth <jakeroth0@gmail.com>
2023-04-11 22:34:21 -0400

    removed mongoose from hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,26 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import ThemeContext from './ThemeContext';
 import { Typewriter } from 'react-simple-typewriter'
-import { PromiseProvider } from 'mongoose';
 
 
 
 function Hero() {
   const { darkMode } = useContext(ThemeContext);
-  // const words = ['BEAUTIFUL', 'ENGAGING', 'CREATIVE', 'FUN'];
-  // const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  // const [currentWord, setCurrentWord] = useState(words[currentWordIndex]);
-
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-  //   }, 2500);
-
-  //   setCurrentWord(words[currentWordIndex]);
-
-  //   return () => clearTimeout(timeout);
-  // }, [currentWordIndex, words]);
-
   return (
     <div className={`${darkMode ? 'graph' : 'bg-white'} xl:flex xl:w-screen xl:px-[8%] xl:justify-center xl:h-[600px]`}>
     <div className={`flex justify-center items-center h-44 pb-8  md:h-auto md:pb-16 xl:justify-start xl:w-[1040px]`}>
