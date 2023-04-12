e0aeda4ad7645ec9e3f540a8ba901a638b7d0d50
Jacob Roth <jakeroth0@gmail.com>
2023-04-11 23:51:22 -0400

    fixed hero desktop spacing and thumbnail

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,8 +7,8 @@ import { Typewriter } from 'react-simple-typewriter'
 function Hero() {
   const { darkMode } = useContext(ThemeContext);
   return (
-    <div className={`${darkMode ? 'graph' : 'bg-white'} xl:flex xl:w-screen xl:px-[8%] xl:justify-center xl:h-[600px]`}>
-    <div className={`flex justify-center items-center h-44 pb-8  md:h-auto md:pb-16 xl:justify-start xl:w-[1040px]`}>
+    <div className={`${darkMode ? 'graph' : 'bg-white'} xl:flex xl:w-screen xl:px-[8%] xl:justify-center xl:h-[450px]`}>
+    <div className={`flex justify-center items-center h-44 pb-8  md:h-auto md:pb-16 xl:pt-24 xl:justify-start xl:w-[1040px]`}>
       <h1 className={`text-5xl flex flex-col h-[145px] md:h-[288px] ${darkMode ? 'text-white' : 'text-black'} md:text-8xl xl:text-8xl`}>
         LET'S MAKE
         <br />
